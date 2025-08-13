3eb7f752b1f890e8ed9718434137794e32b93ec2
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-08-13 05:55:48 +0530

    Create Ex2.java

---
+++ Ex2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Ex2 {
+
+    // shatrudhan.web.developer
+
+    public static void main(String[] args) {
+
+        // Creat a scanner objectfor reading input from consol.
+        Scanner sc = new Scanner(System.in);
+
+        //
+
+    }
+}
