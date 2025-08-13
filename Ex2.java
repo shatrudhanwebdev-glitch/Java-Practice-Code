8c694f818f675cce0e5e6def5f964254c6f5bca7
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-08-13 06:17:35 +0530

    Update Ex2.java

--- Ex2.java
+++ Ex2.java
@@ -4,12 +4,24 @@ public class Ex2 {
 
     // shatrudhan.web.developer
 
+    // Ek string lo or uska reverse print karo.
+
     public static void main(String[] args) {
 
         // Creat a scanner objectfor reading input from consol.
         Scanner sc = new Scanner(System.in);
 
-        //
+        // Scanner object creat
+        System.out.print("Enter your name  : ");
+        String name = sc.nextLine();
 
+        // Name reverse karna
+        String reverse = "";
+        for (int i = name.length() - 1; i >= 0; i--) {
+            reverse += name.charAt(i);
+        }
+
+        // Output print karna
+        System.out.println("Reverse name = " + reverse);
     }
 }
