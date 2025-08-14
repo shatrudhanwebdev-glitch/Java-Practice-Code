09718c310fe51a128b885a4a11155b98250f0a5e
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-08-14 23:36:10 +0530

    yes

--- Variables in java Question/Example2.java
+++ Variables in java Question/Example2.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Example2 {
 
@@ -14,14 +14,14 @@ public class Example2 {
         Scanner sc = new Scanner(System.in);
 
         // Input radius from user
-        System.out.println("Enter radiu : ");
+        System.out.print("Enter radiu : ");
         double radius = sc.nextDouble();
 
         // Condition
         double Area = 3.14 * radius * radius;
         double Circumference = 2 * 3.14 * radius;
 
-        System.out.println("Area of circle + " + Area);
+        System.out.println("Area of circle = " + Area);
         System.out.println("Value of circumference = " + Circumference);
 
     }
