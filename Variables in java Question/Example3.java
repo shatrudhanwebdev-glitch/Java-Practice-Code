77d956efd21d556b0e2ab5cf132499cc244d3a76
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-08-15 07:05:57 +0530

    yes

---
+++ Variables in java Question/Example3.java
@@ -0,0 +1,12 @@
+public class Example3 {
+
+    // shatrudhan.web.developer
+
+    public static void main(String[] args) {
+
+        /*
+         * Write a java program to take single character input from the user and print
+         * its ASCII value.
+         */
+    }
+}
