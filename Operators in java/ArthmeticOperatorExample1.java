83cae103bc1b77b23ab8e016907d56cf1563c434
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-08-16 08:26:56 +0530

    yes

---
+++ Operators in java/ArthmeticOperatorExample1.java
@@ -0,0 +1,28 @@
+public class ArthmeticOperatorExample1 {
+
+    // shatrudhan.web.developer
+
+    public static void main(String args[]) {
+
+        /*
+         * Arthmetic Operators
+         * + (Addition)
+         * - (Subtraction)
+         * * (Muntiplication)
+         * % (Modles)
+         */
+
+        int x = 10, y = 5;
+        int sum, sub, mult, div, mod;
+
+        sum = x + y;
+        sub = x - y;
+        mult = x * y;
+        mod = x % y;
+
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(mult);
+        System.out.println(mod);
+    }
+}
