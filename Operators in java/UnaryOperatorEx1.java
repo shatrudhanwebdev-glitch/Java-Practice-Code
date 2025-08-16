84aadd0bd39b45929a583976c42231d3551a5f90
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-08-16 07:44:19 +0530

    yes

---
+++ Operators in java/UnaryOperatorEx1.java
@@ -0,0 +1,16 @@
+public class UnaryOperatorEx1 {
+
+    // shatrudhan.web.developer
+
+    public static void main(String[] args) {
+
+        // Increment Operators
+        int x = 10;
+        System.out.println(++x); // pre-increment
+        System.out.println(x++); // post-incre,ent
+
+        // Decrement Operators
+        System.out.println(--x); // pre-decremenr
+        System.out.println(x--); // post-decrement
+    }
+}
