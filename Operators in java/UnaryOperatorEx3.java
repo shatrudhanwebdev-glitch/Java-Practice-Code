b25f3ea33f813271edc610ecd4810838c1709859
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-08-17 06:32:45 +0530

    yes

---
+++ Operators in java/UnaryOperatorEx3.java
@@ -0,0 +1,17 @@
+public class UnaryOperatorEx3 {
+
+    // shatrudhan.web.developer
+
+    public static void main(String[] args) {
+
+        // Unary Operators
+
+        int a = 5;
+        int b = a++ + ++a + --a + a--;
+
+        // Output
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
